d37228a557aa52333a6efa2d2db9765a4af72745
Ivan Strike <ria-2015g@yandex.ru>
2023-11-10 20:05:33 +0300

    Changes

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Program
+{
+    public static void Main(String[] args)
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+
+}
